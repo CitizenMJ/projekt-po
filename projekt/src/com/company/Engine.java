9e6f54069902ff6b9232e41ca1c4881731f74e5f
citizenmj <matijar01@interia.pl>
2021-05-27 02:53:47 +0200

    utworzenie plikow projektu

---
+++ projekt/src/com/company/Engine.java
@@ -0,0 +1,22 @@
+package com.company;
+import com.company.people.*;
+public class Engine {
+
+    public static void main(String[] args) {
+
+    }
+
+    void initialize(){
+    }
+    void run(){
+    }
+    void stop(){
+    }
+
+    void printToFile(){
+
+    }
+
+    static private class Day{
+    }
+}
